3de6e9b8cbe6329668bbebd01bb01dbff3b478f1
shubham saad <77003206+subham-saad@users.noreply.github.com>
2023-08-28 07:32:57 +0530

    new updates2

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -29,15 +29,9 @@ const FeedbackCard = ({
             <span className='blue-text-gradient'>@</span> {name}
           </p>
           <p className='mt-1 text-secondary text-[12px]'>
-            {designation} of {company}
+            {designation} at {company}
           </p>
         </div>
-
-        <img
-          src={image}
-          alt={`feedback_by-${name}`}
-          className='w-10 h-10 rounded-full object-cover'
-        />
       </div>
     </div>
   </motion.div>
