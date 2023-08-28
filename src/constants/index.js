f866e2814a78e8c7963a5a62be7c30adda8a60cb
shubham saad <77003206+subham-saad@users.noreply.github.com>
2023-08-28 07:32:57 +0530

    new updates2

--- src/constants/index.js
+++ src/constants/index.js
@@ -163,27 +163,27 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        "I thought it was impossible to make a website as beautiful as our product, but Shubham proved me wrong.",
       name: "Sadanand Lowanshi",
-      designation: "CE",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
+      designation: "CEO",
+      company: "Hexalalitics",
+
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
+        "I've never met a web developer who truly cares about their clients' success like Shubham does.",
+      name:"Rohit Singh Thakur",
+      designation: " Software Developer",
+      company: "Triblio",
+      
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        "After Shubham optimized our website, our traffic increased by 50%. We can't thank them enough!",
       name: "Lisa Wang",
       designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
+      company: "786 Organization",
+     
     },
   ];
   
