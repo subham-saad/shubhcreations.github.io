c66fd66e6b341761767836dfe1760644bb8754f5
shubham saad <shubham@hosteasy.ai>
2024-07-07 16:45:03 +0530

    optimize

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,110 +1,63 @@
 /* eslint-disable react/no-unknown-property */
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState, useMemo } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import { throttle } from 'lodash';
+import { throttle } from "lodash";
 
 import CanvasLoader from "../Loader";
 
-// const Computers = ({ isMobile }) => {
-//   const computer = useGLTF("./desktop_pc/scene.gltf");
+// Optimize GLTF model loading
+const useOptimizedGLTF = (path) => {
+  return useMemo(() => useGLTF(path), [path]);
+};
 
-//   return (
-//     <mesh>
-//       <hemisphereLight intensity={0.15} groundColor='black' />
-//       <spotLight
-//         position={[-20, 50, 10]}
-//         angle={0.12}
-//         penumbra={1}
-//         intensity={1}
-//         castShadow
-//         shadow-mapSize={1024}
-//       />
-//       <pointLight intensity={1} />
-//       <primitive
-//         object={computer.scene}
-//         scale={isMobile ? 0.7 : 0.75}
-//         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-//         rotation={[-0.01, -0.2, -0.1]}
-//       />
-//     </mesh>
-//   );
-// };
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
-  // const my3d  = useGLTF("./my3d/my3d.gltf")
+  const computer = useOptimizedGLTF("./desktop_pc/scene.gltf");
+
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={isMobile ? [-20, 60, 10] : [-20, 50, 10]}
-        angle={isMobile ? 0.3 : 0.12}  // Adjust light angle for mobile
+        angle={isMobile ? 0.3 : 0.12} // Adjust light angle for mobile
         penumbra={1}
-        intensity={isMobile ? 0.5 : 1}  // Adjust light intensity for mobile
+        intensity={isMobile ? 0.5 : 1} // Adjust light intensity for mobile
         castShadow
-        shadow-mapSize={isMobile ? 512 : 1024}  // Adjust shadow map size for mobile
+        shadow-mapSize={isMobile ? 512 : 1024} // Adjust shadow map size for mobile
       />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.5 : 0.75}  // Adjust scale for mobile
+        scale={isMobile ? 0.5 : 0.75} // Adjust scale for mobile
         position={isMobile ? [0, -2, -2] : [0, -3.25, -1.5]}
-        rotation={isMobile ?[-0.02, -0.2, -0.1]:[-0.01, -0.2, -0.1]}
+        rotation={isMobile ? [-0.02, -0.2, -0.1] : [-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
 
-
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
-  
 
-//   useEffect(() => {
-//     // Add a listener for changes to the screen size
-//     const mediaQuery = window.matchMedia("(max-width: 600px)");
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-//     // Set the initial value of the `isMobile` state variable
-//     setIsMobile(mediaQuery.matches);
+    setIsMobile(mediaQuery.matches);
 
-//     // Define a callback function to handle changes to the media query
-//     const handleMediaQueryChange = (event) => {
-//       setIsMobile(event.matches);
-//     };
+    const handleMediaQueryChangeThrottled = throttle((event) => {
+      setIsMobile(event.matches);
+    }, 200);
 
-//     // Add the callback function as a listener for changes to the media query
-//     mediaQuery.addEventListener("change", handleMediaQueryChange);
+    mediaQuery.addEventListener("change", handleMediaQueryChangeThrottled);
 
-//     // Remove the listener when the component is unmounted
-//     return () => {
-//       mediaQuery.removeEventListener("change", handleMediaQueryChange);
-//     };
-//   }, []);
-useEffect(() => {
-  // Add a listener for changes to the screen size
-  const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-  // Set the initial value of the `isMobile` state variable
-  setIsMobile(mediaQuery.matches);
-
-  // Define a throttled callback function to handle changes to the media query
-  const handleMediaQueryChangeThrottled = throttle((event) => {
-    setIsMobile(event.matches);
-  }, 200); // Adjust the throttle duration as needed (e.g., 200 milliseconds)
-
-  // Add the throttled callback function as a listener for changes to the media query
-  mediaQuery.addEventListener("change", handleMediaQueryChangeThrottled);
-
-  // Remove the listener when the component is unmounted
-  return () => {
-    mediaQuery.removeEventListener("change", handleMediaQueryChangeThrottled);
-  };
-}, []);
-
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChangeThrottled);
+    };
+  }, []);
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
